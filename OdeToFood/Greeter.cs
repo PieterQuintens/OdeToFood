f97e17fcc2a980e18440fdfc819293a0b661e725
PieterQuintens <pieter.quintens@hotmail.com>
2019-03-05 10:18:00 +0100

    first commit

---
+++ OdeToFood/Greeter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public class Greeter: IGreeter
+    {
+
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string getMessageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}
